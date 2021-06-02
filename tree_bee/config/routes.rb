21d1e730b18cc92f0bcaba7584f26e0d22d065be
ziosanfer <ziosanfer@gmail.com>
2021-06-02 12:14:32 -0700

    added user api

--- tree_bee/config/routes.rb
+++ tree_bee/config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+  namespace :api do
+    get 'users/create'
+  end
+  get 'sessions/create'
+  get 'sessions/new'
+  get 'sessions/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api, defaults: {format: :json} do
+    resources :user, only: [:create]
+    resource :session, only: [:create, :destroy, :show]
+  end
+  root "static_pages#root"
 end
---
+++ tree_bee/frontend/components/root.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Root = (store) => {
+    <div>Find The Perfect Tree To Vacation In!</div>
+}
+
+export default Root;
